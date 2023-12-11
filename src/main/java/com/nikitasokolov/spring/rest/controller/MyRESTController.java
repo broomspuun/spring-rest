b0d47eaafeef6e109adb3294608d5fcee0e63df0
broomspun44 <nikitasokolov44@mail.ru>
2023-12-11 16:21:28 +0300

    crud operations

--- src/main/java/com/nikitasokolov/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/nikitasokolov/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,9 @@
 package com.nikitasokolov.spring.rest.controller;
 
 import com.nikitasokolov.spring.rest.entity.Employee;
-import com.nikitasokolov.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.nikitasokolov.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.nikitasokolov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,4 +29,23 @@ public class MyRESTController {
         return employee;
     }
 
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee){
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id){
+        Employee employee = employeeService.getEmployee(id);
+        if(employee==null)
+            throw new NoSuchEmployeeException("There is no employee with id = "+ id);
+        employeeService.deleteEmployee(id);
+        return "Employee with id ="+ id +" was deleted";
+    }
 }
